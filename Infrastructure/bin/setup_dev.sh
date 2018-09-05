a95132c825189d5dcb49125a83319882af75c28d
Vivek Varma <vivek@capiot.com>
2018-09-05 12:09:16 +0530

    Edited setup_dev.sh to give proper env variables to mongodb

--- Infrastructure/bin/setup_dev.sh
+++ Infrastructure/bin/setup_dev.sh
@@ -14,7 +14,7 @@ oc project ${GUID}-parks-dev
 
 oc policy add-role-to-user edit system:serviceaccount:70fa-jenkins:jenkins -n 70fa-parks-dev
 
-oc new-app --name=mongodb -e MONGODB_USER=mongodb_user,MONGODB_PASSWORD=mongodb_password,MONGODB_DATABASE=mongodb,MONGODB_ADMIN_PASSWORD=mongodb_admin_password     registry.access.redhat.com/rhscl/mongodb-26-rhel7
+oc new-app --name=mongodb -e MONGODB_USER=mongodb_user -e MONGODB_PASSWORD=mongodb_password -e MONGODB_DATABASE=mongodb -e MONGODB_ADMIN_PASSWORD=mongodb_admin_password     registry.access.redhat.com/rhscl/mongodb-26-rhel7
 # oc pause dc/mongodb 
 echo "apiVersion: "v1"
 kind: "PersistentVolumeClaim"
@@ -31,9 +31,9 @@ oc set volume dc/mongodb --add --overwrite --type=persistentVolume --name=mongo-
 # oc resume dc/mongodb
 
 
-oc new-build --binary=true --name=mlbparks redhat-openjdk18-openshift:1.2 
-oc new-build --binary=true --name=nationalparks redhat-openjdk18-openshift:1.2 
-oc new-build --binary=true --name=parksmap redhat-openjdk18-openshift:1.2 
+oc new-build --binary=true --name=mlbparks openjdk:8 
+oc new-build --binary=true --name=nationalparks openjdk:8
+oc new-build --binary=true --name=parksmap openjdk:8
 
 oc new-app 70fa-parks-dev/mlbparks:0.0-0 -e APPNAME="MLB Parks (Dev)" --name=mlbparks --allow-missing-imagestream-tags=true 
 oc new-app 70fa-parks-dev/nationalparks:0.0-0 -e APPNAME="National Parks (Dev)" --name=nationalparks --allow-missing-imagestream-tags=true 
@@ -52,8 +52,7 @@ echo "DB_HOST=mongodb
 DB_PORT=27017
 DB_USERNAME=mongodb
 DB_PASSWORD=mongodb
-DB_NAME=parks
-" >> application-db.properties
+DB_NAME=parks" > application-db.properties
 
 oc create configmap mlbparks-config --from-literal="application-db.properties=Placeholder"
 oc create configmap nationalparks-config --from-literal="application-db.properties=Placeholder"
@@ -61,14 +60,15 @@ oc create configmap nationalparks-config --from-literal="application-db.properti
 oc env dc/mlbparks --from=configmap/mlbparks-config
 oc env dc/nationalparks --from=configmap/nationalparks-config
 
-oc patch dc/mlbparks --patch "spec: { strategy: {type: rolling, rollingParams: {post: {failurePolicy: Abort, execNewPod: {containerName: mlbparks, command: ["curl -XGET localhost:8081/ws/data/load/"]}}}}}"
-oc patch dc/nationalparks --patch "spec: { strategy: {type: rolling, rollingParams: {post: {failurePolicy: Abort, execNewPod: {containerName: nationalparks, command: ["curl -XGET localhost:8081/ws/data/load/"]}}}}}"
+oc patch dc/mlbparks --patch "spec: { strategy: {type: Rolling, rollingParams: {post: {failurePolicy: Abort, execNewPod: {containerName: mlbparks, command: ['curl -XGET http://localhost:8081/ws/data/load/']}}}}}"
+oc patch dc/nationalparks --patch "spec: { strategy: {type: Rolling, rollingParams: {post: {failurePolicy: Abort, execNewPod: {containerName: nationalparks, command: ['curl -XGET http://localhost:8081/ws/data/load/']}}}}}"
 
 oc set probe dc/mlbparks --liveness --failure-threshold 3 --initial-delay-seconds 60 -- echo ok
 oc set probe dc/mlbparks --readiness --failure-threshold 3 --initial-delay-seconds 60 --get-url=http://:8080/ws/healthz/
 
 oc set probe dc/nationalparks --liveness --failure-threshold 3 --initial-delay-seconds 60 -- echo ok
 oc set probe dc/nationalparks --readiness --failure-threshold 3 --initial-delay-seconds 60 --get-url=http://:8080/ws/healthz/
+
 # oc set volume dc/mlbparks --add --name=mlbparks-config-vol --mount-path=/configuration/application-db.properties --sub-path=application-db.properties --configmap-name=mlbparks-config
 # oc set volume dc/nationalparks --add --name=nationalparks-config-vol --mount-path=/configuration/application-db.properties --sub-path=application-db.properties --configmap-name=nationalparks-config
 
